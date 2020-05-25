1a450af12ff0307b383328961af07d13f31f298a
Kaio Gomes <kaiogomes.nave@gmail.com>
2020-05-25 03:46:01 -0300

    Create navigation stack

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,27 @@
  *
  * @format
  */
+import 'react-native-gesture-handler';
 
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import InitialScreen from './screens/InitialScreen';
 
+const Stack = createStackNavigator();
+
 const App = () => {
-  return <InitialScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="InitialScreen">
+        <Stack.Screen
+          name="InitialScreen"
+          component={InitialScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 export default App;
